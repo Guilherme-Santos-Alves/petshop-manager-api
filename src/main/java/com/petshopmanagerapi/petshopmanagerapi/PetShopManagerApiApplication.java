4f69eaa6ad8d3070b8f7cf33cc81d862ced188cd
Guilherme <guilhermesantosalves068@gmail.com>
2025-05-17 18:18:09 -0300

    projeto inicializado

---
+++ src/main/java/com/petshopmanagerapi/petshopmanagerapi/PetShopManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.petshopmanagerapi.petshopmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetShopManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetShopManagerApiApplication.class, args);
+	}
+
+}
